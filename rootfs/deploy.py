bbe29d170ce10eb0ce6d18f0465e6ec7464f4bdb
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-25 14:41:45 +0000

    Adding tarfile member sanitization to extractall()

--- rootfs/deploy.py
+++ rootfs/deploy.py
@@ -94,7 +94,26 @@ if tar_path:
 
 log("download tar file complete")
 with tarfile.open("apptar", "r:gz") as tar:
-    tar.extractall("/app/")
+    def is_within_directory(directory, target):
+        
+        abs_directory = os.path.abspath(directory)
+        abs_target = os.path.abspath(target)
+    
+        prefix = os.path.commonprefix([abs_directory, abs_target])
+        
+        return prefix == abs_directory
+    
+    def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+    
+        for member in tar.getmembers():
+            member_path = os.path.join(path, member.name)
+            if not is_within_directory(path, member_path):
+                raise Exception("Attempted Path Traversal in Tar File")
+    
+        tar.extractall(path, members, numeric_owner=numeric_owner) 
+        
+    
+    safe_extract(tar, "/app/")
 log("extracting tar file complete")
 buildargs = json.loads(os.getenv('DOCKER_BUILD_ARGS', '{}'))
 # inject docker build args into the Dockerfile so we get around Dockerfiles that don't have things
